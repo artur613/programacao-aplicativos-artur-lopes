0e2b83e1a8b0f2545edfc83d5adbc5731c81c676
artur613 <arturlopesdeoliveira18@gmail.com>
2025-10-07 20:25:38 -0300

    Add files via upload

---
+++ Atividade01_eduardo_amorim_e_artur_lopes.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade01_eduardo_amorim_e_artur_lopes;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade01_eduardo_amorim_e_artur_lopes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int A = 25;
+        int B = A * 3;
+        int C = 10;
+        double D = (double) A / C;
+
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+        System.out.println("C = " + C);
+        System.out.println("D = " + D);
+    }
+    
+}
